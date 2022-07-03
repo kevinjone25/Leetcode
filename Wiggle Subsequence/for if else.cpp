405c6475d8ae70698d514429eb82a51a77f7867c
charleswcz <charleschang907@gmail.com>
2022-07-03 20:33:07 +0800

    new sol

---
+++ Wiggle Subsequence/for if else.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {//other guys sol
+        int inc = 1, dec = 1;
+        for(int i=1; i< nums.size(); i++)
+        {
+            if(nums[i] > nums[i-1]) 
+                inc = dec+1;
+            else if(nums[i] < nums[i-1])
+                dec = inc+1;
+        }
+        return max(inc, dec);
+    }
+};
