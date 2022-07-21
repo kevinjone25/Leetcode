d5f606997a15a3e7a27ae166ba78d3de99d775c0
charleswcz <charleschang907@gmail.com>
2022-07-21 14:20:12 +0800

    reverse linklist

---
+++ Reverse Linked List II/insert node.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+    public:
+        ListNode* reverseBetween(ListNode *head, int left, int right){
+            ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
+            dummy->next = head;
+            for(int i = 0; i < left - 1; ++i)
+                pre = pre->next;
+            cur = pre->next;
+            for(int i = 0; i < right - left; ++i)
+            {
+                ListNode *temp = cur->next;
+                //將curr右邊的點不斷插入到pre後方
+                //到最後就能達成反轉
+                cur->next = temp->next;
+                temp->next = pre->next;
+                pre->next = temp;
+            }
+            return dummy->next;
+        }
+};
