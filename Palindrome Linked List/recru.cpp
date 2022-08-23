81ab0dae0b7d7c353e2f11401c55613535606094
charleswcz <charleschang907@gmail.com>
2022-08-23 17:58:28 +0800

    palindrome link list

---
+++ Palindrome Linked List/recru.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* temp;
+    
+    bool check(ListNode* head) {
+        if(NULL == head)
+            return true;
+        //head是迴文的後面，temp是前面
+        bool res = check(head->next) & (temp->val == head->val);
+        temp = temp->next;
+        return res;
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        temp = head;
+        return check(head);
+    }
+};
