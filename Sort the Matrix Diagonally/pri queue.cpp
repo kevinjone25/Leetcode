a4d115fbf3b36437c852d62558ff887063e71044
charleswcz <charleschang907@gmail.com>
2022-08-28 21:23:17 +0800

    sort mat diagonally

---
+++ Sort the Matrix Diagonally/pri queue.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        map<int, priority_queue<int, vector<int>, greater<int>>>diag;
+        for (int i = 0; i < m; ++i) 
+        {
+            for (int j = 0; j < n; ++j) 
+            {
+                diag[i - j].push(mat[i][j]);
+            }
+        }
+        for(int i = 0; i < m; ++i)
+        {
+            for(int j = 0; j < n; ++j) 
+            {
+                mat[i][j] = diag[i - j].top();
+                diag[i - j].pop();
+            }
+        }
+        return mat;
+    }
+};
